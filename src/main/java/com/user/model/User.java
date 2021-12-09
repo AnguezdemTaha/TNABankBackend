be03d09f502065bf23ab1041a57f7ae59718dbdb
AnguezdemTaha <anguezdemtaha16@gmail.com>
2021-12-09 10:03:42 +0100

    initial commit

---
+++ src/main/java/com/user/model/User.java
@@ -0,0 +1,37 @@
+package com.user.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String nom;
+	
+	public User() {
+		
+	}
+	public User(int id, String nom) {
+		this.id = id;
+		this.nom = nom;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	} 
+
+	
+}
